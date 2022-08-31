04729e2420762893e36020b97668b4cc58e61987
Floschy <69321390+flloschy@users.noreply.github.com>
2022-08-31 19:33:20 +0200

    trigger
    xd

--- nnsave.py
+++ nnsave.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 mnist = keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-#print(x_train.shape, y_train.shape)
+# print(x_train.shape, y_train.shape)
 
 # normalize: 0,255 -> 0,1
 x_train, x_test = x_train / 255.0, x_test / 255.0
